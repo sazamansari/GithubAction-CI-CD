7c9461c2703b8d9848e30dba8d9fecdcb0d5d262
Md Shadab Azam Ansari <34206533+sazamansari@users.noreply.github.com>
2025-05-06 17:49:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
    <div>
     <h1>My React App.</h1>
-  <h2> md first CICD PIPELINES ON GITHUB ACTION</h2>
+  <h2> md first CICD PIPELINES ON GITHUB ACTION new </h2>
    </div>
         
   )
